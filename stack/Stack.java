87ceb02b886d3316fb9e5b9a6e33327e011e437d
drisyaathul <drisyak809@gmail.com>
2023-08-07 02:49:04 +0800

    Added: Create a Stack of 56,30,70

--- stack/Stack.java
+++ stack/Stack.java
@@ -1,4 +1,19 @@
 package com.linkedlistdatastructure.stack;
 
-public class Stack {
+import com.linkedlistdatastructure.LinkedList;
+
+public class Stack <T extends Comparable<T>>{
+    //Creates LinkedList Object
+    LinkedList<T> linkedList = new LinkedList<T>();
+
+    public void push(T data) {
+        /*
+        Here push will internally call add method on LinkedList.
+         */
+        linkedList.add(data);
+    }
+
+    public void display() {
+        linkedList.display();
+    }
 }
--- stack/StackMain.java
+++ stack/StackMain.java
@@ -1,4 +1,14 @@
 package com.linkedlistdatastructure.stack;
 
 public class StackMain {
+    public static void main(String[] args) {
+        //UC1: Create a Stack of 56->30->70.
+        Stack <Integer> stack = new Stack<>();
+        stack.push(70);
+        stack.push(30);
+        stack.push(56);
+        stack.display();
+
+
+    }
 }
