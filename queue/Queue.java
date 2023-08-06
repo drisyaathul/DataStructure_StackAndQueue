fcc2a5a6d07db5085d99a63a1310e52f60f83d33
drisyaathul <drisyak809@gmail.com>
2023-08-07 03:23:03 +0800

    Added: Queue-Data Structure

---
+++ queue/Queue.java
@@ -0,0 +1,4 @@
+package com.linkedlistdatastructure.queue;
+
+public class Queue {
+}
---
+++ queue/QueueMain.java
@@ -0,0 +1,7 @@
+package com.linkedlistdatastructure.queue;
+
+public class QueueMain {
+    public static void main(String[] args) {
+        
+    }
+}
