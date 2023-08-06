64ee1add56faf9d70f55024ab87bd8a4617bc69e
drisyaathul <drisyak809@gmail.com>
2023-08-07 03:31:03 +0800

    Added: Create a Queue using LinkedList

--- queue/Queue.java
+++ queue/Queue.java
@@ -1,4 +1,17 @@
 package com.linkedlistdatastructure.queue;
 
-public class Queue {
+import com.linkedlistdatastructure.LinkedList;
+
+public class Queue <T extends Comparable<T>>{
+
+    //Creates LinkedList Object
+    LinkedList<T> linkedList = new LinkedList<T>();
+
+    public void enqueue(T data) {
+        linkedList.append(data);
+    }
+
+    public void display() {
+        linkedList.display();
+    }
 }
--- queue/QueueMain.java
+++ queue/QueueMain.java
@@ -2,6 +2,12 @@ package com.linkedlistdatastructure.queue;
 
 public class QueueMain {
     public static void main(String[] args) {
-        
+        //UC1: Create a Queue of 56,30,70.
+        Queue <Integer> queue = new Queue<>();
+        queue.enqueue(56);
+        queue.enqueue(30);
+        queue.enqueue(70);
+
+        queue.display();
     }
 }
